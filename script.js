873449b4296a26acc0c95ae549f6c2ac0b2ee9f9
Jaeheon Shim <git@jaeheonshim.com>
2020-01-22 22:16:37 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+today=new Date();
+var cmas=new Date(today.getFullYear(), 11, 25);
+if (today.getMonth()==11 && today.getDate()>25) 
+{
+cmas.setFullYear(cmas.getFullYear()+1); 
+}  
+var one_day=1000*60*60*24;
+var date = Math.ceil((cmas.getTime()-today.getTime())/(one_day));
+
+var bar = new ProgressBar.Line(container, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'},
+  text: {
+    style: {
+      // Text color.
+      // Default: same as stroke color (options.color)
+      color: '#999',
+      position: 'absolute',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: false
+  },
+  from: {color: '#FFEA82'},
+  to: {color: '#ED6A5A'},
+  step: (state, bar) => {
+    bar.setText(Math.round(bar.value() * 100) + ' %');
+  }
+});
+
+bar.animate((365 - date) / 365);  // Number from 0.0 to 1.0
+
+document.getElementById("title").innerHTML = "Year of " + new Date().getFullYear();
+document.getElementById("daysLeft").innerHTML = date + " days left until " + (new Date().getFullYear() + 1)
